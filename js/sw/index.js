a63279ab559b7155435151d6e3679998ba9f8aa5
codeman27 <codeman2727@gmail.com>
2018-08-28 19:11:27 -0600

    working on Service Worker

--- js/sw/index.js
+++ js/sw/index.js
@@ -1,1 +1,31 @@
-console.log('Hello');
+var CACHE_NAME = 'restaurants-static-v2';
+var urlsToCache = [
+  '/',
+  '/js/main.js',
+  '/js/dbhelper.js',
+  '/js/restaurant_info.js',
+  '/css/styles.css',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/1.jpg'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
