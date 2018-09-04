5ce8bf164418ed1e8dc7516dd7d2a4cd548f5628
codeman27 <codeman2727@gmail.com>
2018-09-03 21:12:05 -0600

    Implemented suggested changes

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
 var CACHE_NAME = 'restaurants-static-v2';
 var urlsToCache = [
   '/',
-  //'/js/main.js',
-  //'/js/dbhelper.js',
-  //'/js/restaurant_info.js',
-  //'/css/styles.css',
-  //'/data/restaurants.json',
+  '/js/main.js',
+  '/js/dbhelper.js',
+  '/js/restaurant_info.js',
+  '/css/styles.css',
+  '/data/restaurants.json',
+  '/restaurant.html',
+  '/sw.js'
 ];
 
 self.addEventListener('install', function(event) {
@@ -21,6 +23,7 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
+      console.log(response);
       return response || fetch(event.request);
     })
   );
