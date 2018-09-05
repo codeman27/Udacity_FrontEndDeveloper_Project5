ab1b4e79609f701f4d79641aa4d9904ade284ada
codeman27 <codeman2727@gmail.com>
2018-09-04 20:59:31 -0600

    Fixed service worker

--- sw.js
+++ sw.js
@@ -7,7 +7,16 @@ var urlsToCache = [
   '/css/styles.css',
   '/data/restaurants.json',
   '/restaurant.html',
-  '/sw.js'
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg',
 ];
 
 self.addEventListener('install', function(event) {
@@ -22,9 +31,32 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      console.log(response);
-      return response || fetch(event.request);
-    })
-  );
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
